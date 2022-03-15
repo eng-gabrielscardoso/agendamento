87b43e61eff1e22f818840bb511e63fc8f43686f
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-15 17:09:55 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv-safe').config();
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const dbConnection = process.env.DB_CONNECTION;
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+mongoose.connect(dbConnection)
+  .then(() => console.log('Conexão com o banco de dados estabelecida'))
+  .catch((e) => console.error(`Ocorreu um erro durante a conexão com o banco de dados. ${e}`))
+
+app.get('/', (req, res, next) => {
+  res.json({ status: true });
+});
+
+app.listen(port, () => {
+  try {
+    console.log(`Servidor rodando em: http://localhost:${port}`);
+  } catch (e) {
+    console.error(`Um erro ocorreu durante o processo. ${e}`);
+  }
+});
