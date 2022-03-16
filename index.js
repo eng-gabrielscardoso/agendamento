6134b0da4973d792e03caaed3892fa1fd5080213
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-16 13:23:12 -0300

    feat: add create route

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 const dbConnection = process.env.DB_CONNECTION;
 const port = process.env.PORT || 3000;
 
+const AppointmentService = require('./services/AppointmentService');
+
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,9 +20,33 @@ mongoose.connect(dbConnection)
   .then(() => console.log('Conexão com o banco de dados estabelecida'))
   .catch((e) => console.error(`Ocorreu um erro durante a conexão com o banco de dados. ${e}`))
 
-app.get('/', (req, res, next) => {
+app.get('/', async (req, res, next) => {
   res.render('create');
 });
+
+app.post('/create', async (req, res, next) => {
+  try {
+    const status = await AppointmentService.create(
+      req.body.pacientName,
+      req.body.pacientEmail,
+      req.body.pacientCPF,
+      req.body.pacientDescription,
+      req.body.pacientAppointmentDate,
+      req.body.pacientAppointmentTime,
+    );
+    
+    if (status) {
+      console.log('Registro efetuado com sucesso');
+      res.redirect('/');
+    } else {
+      console.log(`Um erro ocorreu durante a requisição`);
+    res.redirect('/');
+    }
+  } catch (e) {
+    console.log(`Um erro ocorreu durante a requisição de salvamento. Log: ${e}`);
+    res.redirect('/');
+  }
+})
 
 app.listen(port, () => {
   try {
