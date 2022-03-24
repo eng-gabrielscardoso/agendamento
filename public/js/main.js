8201f9498432e9a32ffc4933481dca98e7dffa61
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-24 17:31:48 -0300

    feat: integration full calendar with factory

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,7 @@ const calendarTarget = document.querySelector('div#fullCalendar');
 const calendar = new FullCalendar.Calendar(calendarTarget, {
   initialView: 'dayGridMonth',
   locale: 'pt-br',
+  events: 'getcalendar',
 });
 
 calendar.render();
-
-$(function () {
-  $('#pacientCPF').mask('000.000.000.-00', { reverse: true, });
-});
